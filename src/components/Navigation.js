80b52f325f1dbf3a587c5f14175eed47adbc6567
Jamie Doyle <thedoyles922@users.noreply.github.com>
2017-08-07 13:10:16 -0400

    set up boilerplate

---
+++ src/components/Navigation.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import {Link, NavLink} from 'react-router-dom';
+
+export default class Navigation extends Component {
+  constructor() {
+    super();
+  };
+
+  render() {
+    return(
+      <div>
+
+      </div>
+    );
+  };
+};
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/index.css';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducers from './reducers';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+
+  , document.getElementById('root'));
 registerServiceWorker();
